8b4aec78023ab8cf9e6cc5c9a3f3ffa5a289689b
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-03-06 19:19:43 +0100

    working in the custom hook

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,11 +1,15 @@
-import { useState } from "react"
-import type { OrderItem } from "../types"
+import { useState } from "react";
+import type { MenuItem, OrderItem } from "../types";
 
-export default function useOrder()  {
+export default function useOrder() {
+  const [order, serOrder] = useState<OrderItem[]>([]);
 
-    const [order,serOrder] = useState<OrderItem[]>([])
+  const addItem = (item: MenuItem) => {
+    console.log(item);
+    
+  }
 
-    return {
-
-    }
+  return {
+    addItem
+  };
 }
